98501fe81db45f7f2b32239aa4a7ceef50783df3
Cesar Ivann Llamas Macias <ivannos324@gmail.com>
2025-08-31 13:02:27 -0600

    Agregar campo 'calification' a la estructura 'Libro' y actualizar la función main para incluir calificaciones de los libros. Modificar la impresión para mostrar la calificación junto con el título, autor y género.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ struct Libro {
     titulo: String,
     autor: String,
     genero: String,
-    saga_order: Option<i32>
+    saga_order: Option<i32>,
+    calification: Option<f32>
 }
 
 fn ordenar_libros(libros: &[Libro]) -> Vec<Libro> {
@@ -35,49 +36,64 @@ fn main() {
             titulo: "Ángel mecánico".to_string(),
             autor: "Cassandra Clare".to_string(),
             genero: "Fantasía".to_string(),
-            saga_order: Some(2)
+            saga_order: Some(2),
+            calification: Some(4.5)
         },
         Libro {
             titulo: "Poeta en Nueva York".to_string(),
             autor: "Federico García Lorca".to_string(),
             genero: "Poética".to_string(),
-            saga_order: None
+            saga_order: None,
+            calification: Some(2.0)
         },
         Libro {
             titulo: "Tiempo de alacranes".to_string(),
             autor: "Bernardo Fernández".to_string(),
             genero: "Novela Policiaca".to_string(),
-            saga_order: Some(1)
+            saga_order: Some(1),
+            calification: Some(5.0)
         },
         Libro {
             titulo: "Hielo negro".to_string(),
             autor: "Bernardo Fernández".to_string(),
             genero: "Novela Policiaca".to_string(),
-            saga_order: Some(2)
+            saga_order: Some(2),
+            calification: Some(4.7)
         },
         Libro {
             titulo: "Cuello blanco".to_string(),
             autor: "Bernardo Fernández".to_string(),
             genero: "Novela Policiaca".to_string(),
-            saga_order: Some(3)
+            saga_order: Some(3),
+            calification: Some(4.5)
         },
         Libro {
             titulo: "Azul Cobalto".to_string(),
             autor: "Bernardo Fernández".to_string(),
             genero: "Novela Policiaca".to_string(),
-            saga_order: Some(4)
+            saga_order: Some(4),
+            calification: Some(4.5)
         },
         Libro {
             titulo: "Effortless English: Learn To Speak English Like A Native".to_string(),
             autor: "A.J. Hoge".to_string(),
             genero: "Lenguajes".to_string(),
-            saga_order: None
+            saga_order: None,
+            calification: Some(1.0)
         },
         Libro {
             titulo: "El padrino".to_string(),
             autor: "Mario Puzo".to_string(),
             genero: "Novela".to_string(),
-            saga_order: None
+            saga_order: None,
+            calification: Some(4.0)
+        },
+        Libro {
+            titulo: "Piezas de anime para piano".to_string(),
+            autor: "Varios".to_string(),
+            genero: "Música".to_string(),
+            saga_order: Some(1),
+            calification: None
         },
     ];
 
@@ -85,6 +101,6 @@ fn main() {
 
     println!("🔹 Ordenados:");
     for t in &ordenados {
-        println!("- {} [{}] ({})", t.titulo, t.autor, t.genero);
+        println!("- {} [{}] ({}) - Calificación: {}", t.titulo, t.autor, t.genero, t.calification.map_or("No calificado".to_string(), |c| format!("{:.1}", c)));
     }
 }
